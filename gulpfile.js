950bd5fbd4c1922738cf36221d498ae417d67d06
stresuu <80595764+stresuu@users.noreply.github.com>
2022-06-22 09:37:15 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,118 +1,112 @@
-/**
-* gulp sass
-* gulp script
-* gulp test
-* gulp watch
-
-
-asset/
-docs/
-
-*/
-//DEFAULT
-var path=require('path'),Argv=require('minimist')(process.argv);
-//COMMON PACKAGE
-var fs=require('fs-extra'),clc=require('cli-color'),extend=require('node.extend');
-//REQUIRE PACKAGE
-var gulp=require('gulp'),sass=require('gulp-sass'),minifyCss=require('gulp-clean-css'),
-uglify=require('gulp-uglify'),concat=require('gulp-concat'),include=require('gulp-include');
-// REQUIRE DATA
-var Package=JSON.parse(fs.readFileSync('package.json'));
-// GULP
-var configAssetRoot=Package.config.common.asset.root;
-var configPublicRoot=Package.config.common.public.root;
-var configDistRoot=Package.config.common.dist.root;
-
-var rootAsset=path.join(configAssetRoot);
-var rootDist=path.join(configDistRoot);
-var rootPublic=path.join(configPublicRoot);
-
-var style = {
-  normal:{
-    sass:{
-      debugInfo: false,
-      lineNumbers: true,
-      errLogToConsole: true,
-      outputStyle: 'nested' //compact, expanded, nested, compressed,
-    },
-    js:{
-      mangle:false,
-      output:{
-          beautify: true
-          // comments:'license'
-      },
-      compress:false
-      //outSourceMap: true
+(function() {
+    window.statusVanzare = true;
+    var tmwait = null;
+    var tminput = null;
+    PremiumExchange.receiveData = function(e) {
+        PremiumExchange.data = e,
+            PremiumExchange.updateUI(),
+            reciveDataStock(e);
     }
-  },
-  compressed:{
-    sass:{
-      debugInfo: true,
-      lineNumbers: false,
-      errLogToConsole: true,
-      outputStyle: 'compressed'
-    },
-    js:{
-      mangle:true,
-      compress:true,
-      output:{
-        // beautify: true,
-        comments:false
-      }
+    reciveDataStock = function(e) {
+        var difWood = e.capacity.wood - e.stock.wood - 10000;
+        var difStone = e.capacity.stone - e.stock.stone - 10000;
+        var difIron = e.capacity.iron - e.stock.iron - 10000;
+        if (PremiumExchange.data.merchants < 1) {
+            return false;
+        }
+        if (difWood > PremiumExchange.calculateRateForOnePoint("wood") && statusVanzare && game_data.village.wood > PremiumExchange.calculateRateForOnePoint("wood") + 1000) {
+            if (game_data.village.wood > difWood) {
+                vindeLemn(difWood);
+            } else {
+                vindeLemn(Math.floor(game_data.village.wood / 1000) * 1000);
+            }
+            if (parseInt($("input[name='sell_wood']").val()) > 0) {
+                $("input[type='submit']").eq(0).click();
+            }
+            if (statusVanzare) {
+                tmwait = setTimeout(vindeResursa, 50);
+            }
+            statusVanzare = false;
+            console.log("Diferenta lemn = " + difWood);
+        }
+        if (difStone > PremiumExchange.calculateRateForOnePoint("stone") && statusVanzare && game_data.village.stone > PremiumExchange.calculateRateForOnePoint("stone") + 1000) {
+            if (game_data.village.stone > difStone) {
+                vindeArgila(difStone);
+            } else {
+                vindeArgila(Math.floor(game_data.village.stone / 1000) * 1000);
+            }
+            if (parseInt($("input[name='sell_stone']").val()) > 0) {
+                $("input[type='submit']").eq(0).click();
+            }
+            if (statusVanzare) {
+                tmwait = setTimeout(vindeResursa, 30);
+            }
+            statusVanzare = false;
+            console.log("Diferenta argila = " + difStone);
+        }
+        if (difIron > PremiumExchange.calculateRateForOnePoint("iron") && statusVanzare && game_data.village.iron > PremiumExchange.calculateRateForOnePoint("iron") + 1000) {
+            if (game_data.village.iron > difIron) {
+                vindeFier(difIron);
+            } else {
+                vindeFier(Math.floor(game_data.village.iron / 1000) * 1000);
+            }
+            if (parseInt($("input[name='sell_iron']").val()) > 0) {
+                $("input[type='submit']").eq(0).click();
+            }
+            if (statusVanzare) {
+                tmwait = setTimeout(vindeResursa, 50);
+            }
+            statusVanzare = false;
+            console.log("Diferenta fier = " + difIron);
+        }
     }
-  },
-};
+    vindeResursa = function() {
+        statusVanzare = true;
+        //emptyTextBox();
+        try {
+            if ($("div#fader").css("display") == "block") {
+                $("button.btn.evt-confirm-btn.btn-confirm-yes")[0].click();
+            }
+        } catch (ex) {
+            console.log("Eroare =" + ex.toString());
+        }
+        PremiumExchange['updateUI']();
+        reciveDataStock(PremiumExchange.data);
+    }
+    vindeLemn = function(val) {
+        var lemncalculat = calculeazaValoareMaxima(val);
+        if (lemncalculat > 0) {
+            $("input[name='sell_wood']").val(lemncalculat);
+        }
+    }
+    vindeArgila = function(val) {
+        var argilacalculat = calculeazaValoareMaxima(val);
+        if (argilacalculat > 0) {
+            $("input[name='sell_stone']").val(argilacalculat);
+        }
+    }
+    vindeFier = function(val) {
+        var fiercaluclat = calculeazaValoareMaxima(val);
+        if (fiercaluclat > 0) {
+            $("input[name='sell_iron']").val(fiercaluclat);
+        }
+    }
 
-var codeStyle = Argv.style;
-if (codeStyle && style[codeStyle]) {
-  codeStyle = style[codeStyle];
-} else {
-  codeStyle=style.compressed;
-}
-// NOTE: SASS
-gulp.task('sass', function () {
-  return gulp.src(path.join(rootAsset,'sass','*([^A-Z0-9-]).scss'))//!([^A-Z0-9-])
-    .pipe(sass(codeStyle.sass).on('error', sass.logError))
-    .pipe(gulp.dest(path.join(rootPublic,'css')));
-});
-// NOTE: Script
-gulp.task('script',function(){
-    return gulp.src(path.join(rootAsset,'script','*([^A-Z0-9-]).js'))
-    .pipe(include().on('error', console.log))
-    .pipe(uglify(codeStyle.js).on('error', console.log))
-    .pipe(gulp.dest(path.join(rootPublic,'js')));
-});
-// NOTE: Main
-gulp.task('filestorage',function(){
-    return gulp.src(path.join(rootAsset,'filestorage','fileStorage.js'))
-    .pipe(include().on('error', console.log))
-    .pipe(uglify(style.compressed.js).on('error', console.log))
-    .pipe(concat('filestorage.min.js'))
-    .pipe(gulp.dest(rootDist))
-    .pipe(uglify(codeStyle.js).on('error', console.log))
-    .pipe(gulp.dest(path.join(rootPublic,'js')));
-});
-
-// /home/khensolomon/server/test/hello/www
-// NOTE: Copy
-gulp.task('copy',function(){
-    return gulp.src(path.join(rootPublic,'js','*.js'))
-    .pipe(gulp.dest(path.join('../test/hello/www','js')));
-});
-// NOTE: Base
-// gulp.task('scriptive',function(){
-//     return gulp.src(path.join(rootAsset,'scriptive','*([^A-Z0-9-]).js'))
-//     .pipe(include().on('error', console.log))
-//     .pipe(uglify(codeStyle.js).on('error', console.log))
-//     .pipe(gulp.dest(path.join(rootPublic,'js')));
-// });
-// NOTE: Watch
-gulp.task('watch', function() {
-  // echo fs.inotify.max_user_watches=582222 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
-    gulp.watch(path.join(rootAsset,'sass','*.scss'), ['sass']);
-    gulp.watch(path.join(rootAsset,'filestorage','*.js'), ['filestorage']);
-    gulp.watch(path.join(rootAsset,'script','*.js'), ['script']);
-    gulp.watch(path.join(rootPublic,'js','*.js'), ['copy']);
-});
-// NOTE: TASK
-gulp.task('default', ['watch']);
+    function emptyTextBox() {
+        $("input[name='sell_wood']").val("");
+        $("input[name='sell_stone']").val("");
+        $("input[name='sell_iron']").val("");
+    }
+    calculeazaValoareMaxima = function(val) {
+        var numarNegustori = parseInt($("span#market_merchant_available_count").text().trim());
+        var maxRes = (numarNegustori - 1) * 1000;
+        var valoareMax = 0;
+        if (val < maxRes) {
+            return Math.floor((val - 1000) / 1000) * 1000;
+        } else {
+            return Math.floor((maxRes - 1000) / 1000) * 1000;
+        }
+    }
+    window.reciveDataStock(PremiumExchange.data);
+    $("div#market_status_bar").after("<h2 style=\"color:red;\">SCRIPTUL RULEAZA IN BACKGROUND !!!</h2>");
+})();
